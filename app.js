f6d1a71e4ca883aa1597416170cf061fc2fd1211
Andrej Spasov <andrej@Andrejs-MacBook-Pro.local>
2024-05-01 15:44:13 +0100

    Initial setup

---
+++ app.js
@@ -0,0 +1,32 @@
+// check if running in production env
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const expressLayouts = require('express-ejs-layouts')
+const db = mongoose.connection
+
+const app = express();
+
+const indexRouter = require('./routes/index')
+
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+
+app.use('/', indexRouter)
+
+mongoose.connect(process.env.DATABASE_URL);
+
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('connected to Mongoose'))
+
+
+app.listen(process.env.PORT || 3000)
